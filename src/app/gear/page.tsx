b3d270d931131d44293327da71259782699d2d32
gustavobaltazar <gustavobaltazar31@gmail.com>
2024-11-12 09:43:41 -0300

    Project  build error fixed

--- src/app/gear/page.tsx
+++ src/app/gear/page.tsx
@@ -2,7 +2,7 @@ export default function Gear() {
   return (
     <div className="flex flex-col items-center gap-5 my-12 md:px-[18vw] px-6 ">
       <ul>
-        <li>I didn't clean my desk =P aprecciate this setup instead</li>
+        <li>I didnt clean my desk =P aprecciate this setup instead</li>
       </ul>
       <img src="/setup.jpg" className="w-[20%] rounded-lg" />
     </div>
